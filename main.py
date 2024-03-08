ca4ebd5b1834a62cbcb4063bc4ce2d5d7ee8cde2
LuisLeon-Dev <lleon5073@gmail.com>
2024-03-08 09:06:08 -0600

    add: check the corrects answer functionality

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ question_bank = []
 for data in question_data:
     text = data["text"]
     answer = data["answer"]
-    question = Question(text, data)
+    question = Question(text, answer)
     question_bank.append(question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
